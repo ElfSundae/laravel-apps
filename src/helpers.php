c49555924072759a6d38b528baa22d055ca6a63d
Elf Sundae <elf.sundae@gmail.com>
2017-12-01 03:31:54 +0800

    Add is_app() helper function

--- src/helpers.php
+++ src/helpers.php
@@ -1,1 +1,27 @@
 <?php
+
+if (! function_exists('is_app')) {
+    /**
+     * Determine the current sub application.
+     *
+     * @param  string  $identifiers
+     * @return bool
+     */
+    function is_app(...$identifiers)
+    {
+        $request = app('request');
+        $currentUrl = $request->getHttpHost().$request->getBaseUrl().$request->getPathInfo();
+
+        foreach ($identifiers as $identifier) {
+            if ($url = config('apps.url.'.$identifier)) {
+                $url = preg_replace('#^https?://#', '', $url, 1);
+                $pattern = '#^'.preg_quote($url, '#').'([\?/].*)?$#';
+                if (preg_match($pattern, $currentUrl) === 1) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
