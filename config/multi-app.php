f25477e0396f3384a6fb89657e89c9f35c365fdb
Elf Sundae <elf.sundae@gmail.com>
2017-11-30 23:02:18 +0800

    Add configuration file

--- config/multi-app.php
+++ config/multi-app.php
@@ -4,11 +4,22 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Configuration Name
+    | Application URLs
     |--------------------------------------------------------------------------
     |
-    | description
+    | The root URL for each sub application.
+    |
+    | The domain of each URL will be automatically avaliable in
+    | "multi-app.domain" configuration.
     |
     */
+
+    'url' => [
+        'web' => env('APP_URL'),
+        'admin' => env('APP_URL_ADMIN', env('APP_URL')),
+        'api' => env('APP_URL_API', env('APP_URL')),
+        'assets' => env('APP_URL_ASSETS', env('APP_URL')),
+        'cdn' => env('APP_URL_CDN', env('APP_URL')),
+    ],
 
 ];
