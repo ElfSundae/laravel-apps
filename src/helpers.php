01a5f1b42beba4710f07441a4d09dde7f7de46ca
Elf Sundae <elf.sundae@gmail.com>
2017-12-09 16:42:01 +0800

    Remove app_id() app_url() helpers

--- src/helpers.php
+++ src/helpers.php
@@ -11,30 +11,3 @@ if (! function_exists('apps')) {
         return app('apps');
     }
 }
-
-if (! function_exists('app_id')) {
-    /**
-     * Get or check the current application identifier.
-     *
-     * @return string|bool
-     */
-    function app_id()
-    {
-        return app('apps')->id(...func_get_args());
-    }
-}
-
-if (! function_exists('app_url')) {
-    /**
-     * Generate an absolute URL to a path for the given application identifier.
-     *
-     * @param  string  $appId
-     * @param  string  $path
-     * @param  mixed  $extra
-     * @return string
-     */
-    function app_url($appId = '', $path = '', $extra = [])
-    {
-        return app('apps')->url($appId, $path, $extra);
-    }
-}
