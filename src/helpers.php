8776cae75e6c19e6f5db09cb7088850cd6745627
Elf Sundae <elf.sundae@gmail.com>
2017-12-10 13:16:03 +0800

    Revert "Remove app_id() app_url() helpers"
    This reverts commit 01a5f1b42beba4710f07441a4d09dde7f7de46ca.

--- src/helpers.php
+++ src/helpers.php
@@ -11,3 +11,30 @@ if (! function_exists('apps')) {
         return app('apps');
     }
 }
+
+if (! function_exists('app_id')) {
+    /**
+     * Get or check the current application identifier.
+     *
+     * @return string|bool
+     */
+    function app_id()
+    {
+        return app('apps')->id(...func_get_args());
+    }
+}
+
+if (! function_exists('app_url')) {
+    /**
+     * Generate an absolute URL to a path for the given application identifier.
+     *
+     * @param  string  $app
+     * @param  string  $path
+     * @param  mixed  $parameters
+     * @return string
+     */
+    function app_url($app = '', $path = '', $parameters = [])
+    {
+        return app('apps')->url($app, $path, $parameters);
+    }
+}
--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -2,11 +2,35 @@
 
 namespace ElfSundae\Laravel\Apps\Test;
 
+use Mockery as m;
+
 class HelpersTest extends TestCase
 {
     public function test_apps()
     {
-        $this->registerAppsService();
-        $this->assertSame($this->app['apps'], apps());
+        $this->app->instance('apps', 'foo');
+        $this->assertSame('foo', apps());
+    }
+
+    public function test_app_id()
+    {
+        $apps = m::mock('stdClass');
+        $apps->shouldReceive('id')
+            ->once()
+            ->with('foo', 'bar')
+            ->andReturn('result');
+        $this->app->instance('apps', $apps);
+        $this->assertSame('result', app_id('foo', 'bar'));
+    }
+
+    public function test_app_url()
+    {
+        $apps = m::mock('stdClass');
+        $apps->shouldReceive('url')
+            ->once()
+            ->with('a', 'b', 'c')
+            ->andReturn('result');
+        $this->app->instance('apps', $apps);
+        $this->assertSame('result', app_url('a', 'b', 'c'));
     }
 }
