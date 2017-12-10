10b5bfe903cf3413e6c8f121dc04dac0d598dd60
Elf Sundae <elf.sundae@gmail.com>
2017-12-10 12:14:27 +0800

    test publish assets

--- tests/AppsServiceProviderTest.php
+++ tests/AppsServiceProviderTest.php
@@ -7,6 +7,13 @@ use ElfSundae\Laravel\Apps\Facades\Apps;
 
 class AppsServiceProviderTest extends TestCase
 {
+    public function tearDown()
+    {
+        $this->app['files']->delete(config_path('apps.php'));
+
+        parent::tearDown();
+    }
+
     public function testServiceProvider()
     {
         $this->registerAppsService();
@@ -16,6 +23,12 @@ class AppsServiceProviderTest extends TestCase
         $this->assertInstanceOf(AppManager::class, $manager);
         $this->assertSame($manager, $this->app[AppManager::class]);
         $this->assertSame($manager, Apps::getFacadeRoot());
+
+        $this->artisan('vendor:publish', [
+            '--force' => true,
+            '--provider' => 'ElfSundae\Laravel\Apps\AppsServiceProvider',
+        ]);
+        $this->assertFileExists(config_path('apps.php'));
 
         $this->assertTrue($this->app['url']->hasMacro('getRootControllerNamespace'));
     }
