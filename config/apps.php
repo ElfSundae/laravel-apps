5957cc9e09c26d019ddafe3c5d60d7471fdfa6d7
Elf Sundae <elf.sundae@gmail.com>
2017-12-12 12:11:14 +0800

    Remove cdn app

--- config/apps.php
+++ config/apps.php
@@ -16,7 +16,6 @@ return [
         'admin' => env('APP_URL_ADMIN', env('APP_URL')),
         'api' => env('APP_URL_API', env('APP_URL')),
         'assets' => env('APP_URL_ASSETS', env('APP_URL')),
-        'cdn' => env('APP_URL_CDN', env('APP_URL')),
     ],
 
     /*
