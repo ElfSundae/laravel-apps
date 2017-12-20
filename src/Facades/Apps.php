8272d64acd96cd0cb22131d1519b6c151cfe33a4
Elf Sundae <elf.sundae@gmail.com>
2017-12-20 08:20:01 +0800

    Add docblock to Apps facade

--- src/Facades/Apps.php
+++ src/Facades/Apps.php
@@ -5,6 +5,16 @@ namespace ElfSundae\Laravel\Apps\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static array urls()
+ * @method static string root(string $app = '')
+ * @method static string domain(string $app = '')
+ * @method static string prefix(string $app = '')
+ * @method static string|bool id()
+ * @method static string|null idForUrl(string $url)
+ * @method static static refreshId()
+ * @method static string url(string $app = '', string $path = '', mixed $parameters = [])
+ * @method static void routes(array $attributes = [])
+ *
  * @see \ElfSundae\Laravel\Apps\AppManager
  */
 class Apps extends Facade
